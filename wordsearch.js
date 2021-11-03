7ab24f66e0d58c2db8f6c7aa537c6e516628d8fd
WenConrad <conradwen@gmail.com>
2021-11-03 14:45:41 -0700

    word search core. pairing with @Ayo-Show

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,21 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  console.log(horizontalJoin);
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  let vArray = [];
+  for (let i in letters[0]) {
+    let vWord = "";
+    for (let j in letters) {
+      vWord += letters[j][i];
     }
-}
+    vArray.push(vWord);
+  }
+  for (let l of vArray) {
+    if (l.includes(word)) return true;
+  }
+  return false;
+};
 
-module.exports = wordSearch
+module.exports = wordSearch;
